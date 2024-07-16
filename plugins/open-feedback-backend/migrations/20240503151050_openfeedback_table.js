aaf5a47e9edb36628fad94215ed35a2ad76730a1
Michael Yin <baicheng.yin@samsara.com>
2024-07-16 13:49:47 +0100

    created new database migration to follow best practices

--- plugins/open-feedback-backend/migrations/20240503151050_openfeedback_table.js
+++ plugins/open-feedback-backend/migrations/20240503151050_openfeedback_table.js
@@ -5,7 +5,6 @@
 exports.up = function up(knex) {
   return knex.schema.createTable('open_feedback', table => {
     table.increments('id').primary();
-    table.string('url').notNullable();
     table.string('userRef').notNullable();
     table.string('rating').notNullable();
     table.string('comment');
---
+++ plugins/open-feedback-backend/migrations/20240716124538_add_url_col.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.table('open_feedback', table => {
+    table.string('url');
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.table('open_feedback', table => {
+        table.dropColumn('url');
+    });
+};
