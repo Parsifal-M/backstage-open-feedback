9acc3726c5d255f2b04879d85edf9ba0dc300553
Peter Macdonald <macdonald.peter90@gmail.com>
2025-11-28 16:14:03 +0100

    Version bump (#67)
    * Version bump
    
    Signed-off-by: Peter Macdonald <macdonald.peter90@gmail.com>
    
    * remove react imports
    
    Signed-off-by: Peter Macdonald <macdonald.peter90@gmail.com>
    
    ---------
    
    Signed-off-by: Peter Macdonald <macdonald.peter90@gmail.com>

--- plugins/open-feedback/src/components/OpenFeedbackModal/OpenFeedbackModal.test.tsx
+++ plugins/open-feedback/src/components/OpenFeedbackModal/OpenFeedbackModal.test.tsx
@@ -1,7 +1,7 @@
 import { renderInTestApp, TestApiProvider } from '@backstage/test-utils';
 import { fireEvent, screen } from '@testing-library/react';
 import { alertApiRef } from '@backstage/core-plugin-api';
-import React, { act } from 'react';
+import { act } from 'react';
 import { OpenFeedbackModal } from './OpenFeedbackModal';
 import { openFeedbackBackendRef } from '../../api/types';
 import { usePermission } from '@backstage/plugin-permission-react';
